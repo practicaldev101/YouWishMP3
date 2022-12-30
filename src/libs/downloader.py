82969ee299d65043910bac5b201b07b513747572
practicaldev101 <carlosospinomonsalve@gmail.com>
2022-12-30 18:19:16 -0500

    First deploy

---
+++ src/libs/downloader.py
@@ -0,0 +1,49 @@
+from pytube import YouTube, exceptions
+from pytube.cli import on_progress
+from colorama import Fore
+import os
+from os import path
+class Audio():
+
+    def __init__(self, url: str) -> None:
+        self.url = url
+        self.directory = "downloads"
+
+    def printMessage(self, text: str, color: str) -> None:
+        tempColor = ""
+        if color == "green":
+            tempColor = Fore.LIGHTGREEN_EX
+        elif color == "yellow":
+            tempColor = Fore.LIGHTYELLOW_EX
+        elif color == "red":
+            tempColor = Fore.LIGHTRED_EX
+        print(tempColor + text + Fore.RESET)
+    
+    def downloadAudio(self) -> True or False:
+        try:
+            self.video = YouTube(self.url, on_progress_callback=on_progress)
+            self.printMessage("[ + ] Getting audio from video\n", "green")
+            audio = self.video.streams.get_audio_only(subtype="webm")
+            outputPath =  path.abspath("./" + self.directory)
+            filename = audio.title + ".mp3"
+            absFilePath = path.join(outputPath, filename)
+
+            if path.exists(outputPath):
+                self.printMessage("[ + ] Verifying that the file doesn't exist\n", "yellow")
+
+                if path.exists(absFilePath) == False:
+                    self.printMessage("[ + ] Downloading " + audio.title + "\n", "green")
+                    audio.download(output_path=outputPath, filename=filename)
+                    self.printMessage("\n[ + ] Audio " + audio.title + " downloaded" + "\n", "green")
+                    return True
+                else:
+                    self.printMessage("[ ! ] The file '" + filename + "' exists...Skipping\n", "red")
+                    return False
+            else:
+                self.printMessage("[ ! ] The directory " + self.directory +" doesn't exist, creating new one\n", "yellow")
+                os.mkdir(outputPath)
+                self.downloadAudio()
+        except exceptions.RegexMatchError:
+            self.printMessage("\n[ ! ] It looks like URL video is invalid.\n", "red")
+            self.printMessage("[ ! ] URL: " + self.url + "\n", "yellow")
+            return False;
---
+++ src/main.py
@@ -0,0 +1,38 @@
+from libs.downloader import Audio
+from colorama import Fore
+from os import system, name
+
+def logo() -> None:
+    if name == 'nt':
+        _ = system('cls')
+    else:
+        _ = system('clear')
+    print("""
+__   __          _    _ _     _    ___  _________ _____ 
+\ \ / /         | |  | (_)   | |   |  \/  || ___ \____ |
+ \ V /___  _   _| |  | |_ ___| |__ | .  . || |_/ /   / /
+  \ // _ \| | | | |/\| | / __| '_ \| |\/| ||  __/    \ \\
+  | | (_) | |_| \  /\  / \__ \ | | | |  | || |   .___/ /
+  \_/\___/ \__,_|\/  \/|_|___/_| |_\_|  |_/\_|   \____/                                                         
+    
+    By Practical Programmer | GitHub:practicaldev101
+""")
+
+def main() -> None:
+    try:
+        logo()
+        filename = "music.txt"
+        with open(filename, "r") as file:
+            for url in file.readlines():
+                if url.strip() != "":
+                    Audio(url).downloadAudio()
+            file.close()
+        print(Fore.GREEN + "[ + ] File " + filename + " finished." + Fore.RESET)
+    except FileNotFoundError:
+        print(Fore.YELLOW + "[ ! ] File " + filename + " not found, creating new one" + Fore.RESET)
+        with open(filename, "w") as file: file.close()
+        print(Fore.YELLOW + "[ ! ] Please, fill the file and try again" + Fore.RESET)
+
+
+if __name__ == "__main__":
+    main()
